4d55f77f7149dbfbb390e4f4d2c63c4864a07908
Zsolti <zsolt.zombori11@gmail.com>
2021-02-12 16:45:43 +0100

    Initial files

---
+++ src/testing.java
@@ -0,0 +1,9 @@
+public class testing {
+
+    public static void main(String[]args){
+        Coworker worker = new Coworker("Test", "TX-Edge", "Me", 40);
+        System.out.println(worker.getId());
+    }
+}
+
+
